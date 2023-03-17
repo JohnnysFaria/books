07abf69e7d458baa89fb051730d1e6b2a643c11b
= <johnnys.faria@gmail.com>
2023-03-17 12:29:19 -0300

    Finalização da barra de pesquisa

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,54 +1,70 @@
 import Input from '../Input'
-import { useState } from 'react'
 import styled from 'styled-components'
+import { useState } from 'react'
 import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
-        background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
-        color: #FFF;
-        text-align: center;
-        padding: 85px 0;
-        height: 270px;
-        width: 100%;
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
+    color: #FFF;
+    text-align: center;
+    padding: 85px 0;
+    height: 270px;
+    width: 100%;
 `
 
 const Titulo = styled.h2`
-        color: #FFF;
-        font-size: 36px;
-        text-align: center;
-        width: 100%;
+    color: #FFF;
+    font-size: 36px;
+    text-align: center;
+    width: 100%;
 `
 
 const Subtitulo = styled.h3`
-        font-size: 16px;
-        font-weight: 500;
-        margin-bottom: 40px;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 40px;
 `
 
-const Index = () => {
-    const [livroPesquisado, setLivrosPesquisado] = useState(null);
-    
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
+function Pesquisa() {
+    const [ livrosPesquisados, setLivrosPesquisados ] = useState([])
+
     return (
         <PesquisaContainer>
-        <Titulo>Já sabe por onde começar?</Titulo>
-        <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
-        <Input 
-            placeholder='Escreva sua próxima leitura'
-            onBlur={event => {
-                const textoDigitado = event.target.value
-                const resultado = livros.filter(livro => livro.nome.includes(textoDigitado))
-                setLivrosPesquisado(resultado)
-            }}
-        />
-        { livroPesquisado.map( livro => (
-                <div>
-                 <p>{livro.nome}</p>
-                 <img src={livro.src}/>
-
-                </div>
-                ))}
-    </PesquisaContainer>
-  )
+            <Titulo>Já sabe por onde começar?</Titulo>
+            <Subtitulo>Encontre seu livro em nossa estante.</Subtitulo>
+            <Input
+                placeholder="Escreva sua próxima leitura"
+                onBlur={evento => {
+                    const textoDigitado = evento.target.value
+                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado) )
+                    setLivrosPesquisados(resultadoPesquisa)
+                }}
+            />
+            { livrosPesquisados.map( livro => (
+                <Resultado>
+                    <img src={livro.src}/>
+                    <p>{livro.nome}</p>
+                </Resultado>
+            )) }
+        </PesquisaContainer>
+    )
 }
 
-export default Index
+export default Pesquisa
